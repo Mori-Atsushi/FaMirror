ef07e7e92eb8fea8fc83955a5e700745156761ee
森篤史 <tb.e.zizii82@gmail.com>
2015-10-22 12:08:43 +0900

    鏡サンプル完成

---
+++ js/mirror.js
@@ -0,0 +1,11 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+
+var mirror = document.getElementById('mirror');
+var localStream = null;
+navigator.getUserMedia({video: true, audio: false},
+	function(stream) {
+		mirror.src = window.URL.createObjectURL(stream);
+	}, function(err) {
+		console.log(eer);
+	});
